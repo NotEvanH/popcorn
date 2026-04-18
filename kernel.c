eef50626b739a78b34be8a23f5da3120fbc048b9
Evan Ho <=>
2026-04-18 10:22:46 +0800

    Added new lines characters to kernel.

--- kernel.c
+++ kernel.c
@@ -1,3 +1,18 @@
+const int MAX_HEIGHT = 80;
+const int MAX_WIDTH = 25;
+
+int get_row_from_offset(int offset) {
+    return offset / (2 * MAX_HEIGHT);
+}
+
+int get_offset(int col, int row) {
+    return 2 * (row * MAX_HEIGHT + col);
+}
+
+int move_offset_to_new_line(int offset) {
+    return get_offset(0, get_row_from_offset(offset) + 1);
+}
+
 void print(const char* string) {
     char* video = (char*) 0xB8000; // 0xB8000 = VGA text buffer
 
@@ -6,13 +21,19 @@ void print(const char* string) {
 
     // VGA Text Structure: [character][colour][character][colour]
     while (string[i] != '\0') {
-        video[offset] = string[i];
-        video[offset + 1] = 0x07;
+        char character = string[i];
+        if (character == '\n') {
+            offset = move_offset_to_new_line(offset);
+        } else {
+            video[offset] = character;
+            video[offset + 1] = 0x07;
+            offset += 2;
+        }
+
         i++;
-        offset += 2;
     }
 }
 
 void kernel_main() {
-    print("Hello World!");
+    print("Hello,\nWorld!");
 }
